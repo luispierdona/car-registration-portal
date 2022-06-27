e55843ded0d236d6c6e85f4c1b54e7bcae40974d
Luis Matias Pierdona <luismathias.pierdona@de.adecco.net>
2022-06-27 12:02:15 +0200

    Cars list, dashaboard and service with error handling added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from "@angular/flex-layout";
 
 import { DashboardComponent } from './modules/dashboard/components/dashboard/dashboard.component';
 import { LoginComponent } from './modules/login/components/login/login.component';
+import { DashboardService } from './modules/dashboard/services/dashboard.service';
 
 import { ToolbarComponent } from './modules/dashboard/components/toolbar/toolbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -23,13 +27,20 @@ import { MatButtonModule } from '@angular/material/button';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
+    /*
+      @FlexLayoutModule 14 for Angular 14 is not ready yet.
+      So used: npm install --save --legacy-peer-deps @angular/flex-layout
+    */
+    FlexLayoutModule,
 
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatSnackBarModule
   ],
-  providers: [],
+  providers: [DashboardService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/modules/dashboard/components/dashboard/dashboard.component.ts
+++ src/app/modules/dashboard/components/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+import { Car } from '../../models/cars.models';
+import { DashboardService } from '../../services/dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  gridColumns = 4;
+  cars: Car[] = [];
+  constructor(private dashboardService: DashboardService, private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+    // Just to show the Loading
+    setTimeout(() => { this.getCars(); }, 1500);
+  }
+
+  getCars() {
+    this.dashboardService.loadCars().subscribe({
+      next: (cars: Car[]) => {
+        this.cars = cars;
+      },
+      error: (err) => {
+        this.openSnackBar(err?.message, 'Close',
+        { duration: 2000, panelClass: ['mat-toolbar', 'mat-warn'] });
+      },
+      complete: () =>  this.openSnackBar('Cars loaded successfully', 'Close',
+        { duration: 2000, panelClass: ['mat-toolbar', 'mat-primary'] })
+    });
+  }
+
+  openSnackBar(message: string, action: string, config: MatSnackBarConfig) {
+    this._snackBar.open(message, action, config);
   }
 
 }
